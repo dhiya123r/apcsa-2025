dd0380b180da6929b6c31cb572f20a83f99a260b
dhiya123r <dr2458@pleasantonusd.net>
2025-09-28 01:11:48 +0000

    basic world code finished

--- robot/Roomba.java
+++ robot/Roomba.java
@@ -3,45 +3,125 @@ package robot;
 import kareltherobot.*;
 
 public class Roomba implements Directions {
+	private Robot roomba;
+	//Statistics we want to report
+	private int area = 0;
+	private int piles = 0; 
+	private int totalBeepers = 0;
+	private int largestPile = 0; 
+	private int largestX = 0;
+	private int largestY =0;
 
-	// Main method to make this self-contained
-	public static void main(String[] args) {
-		// LEAVE THIS ALONE!!!!!!
-		String worldName = "robot/basicRoom.wld";
+public static void main(String[] args) {
+	String worldName = "robot/basicRoom.wld";
+	Roomba cleaner = new Roomba();
+	cleaner.cleanRoom(worldName, 7, 6);
+	cleaner.report();
+}
+		
+public void cleanRoom(String worldName, int startX, int startY) {
 
-		Roomba cleaner = new Roomba();
-		int totalBeepers = cleaner.cleanRoom(worldName, 7, 6);
-		System.out.println("Roomba cleaned up a total of " + totalBeepers + " beepers.");
+	World.readWorld(worldName);
+	World.setVisible(true);
+	World.setDelay(1);
 
+	roomba = new Robot(startX, startY, West, 0);
+	
+	while (roomba.frontIsClear()) {
+		roomba.move();
+	}
+	roomba.turnLeft();
+
+	while (roomba.frontIsClear()) {
+		roomba.move();
+	}
+	roomba.turnLeft();
+
+	//Determine room width and height
+	int width = 0; 
+	int height = 0;
+    while (roomba.frontIsClear()) {
+		width++; 
+		roomba.move();
+	}
+    roomba.turnLeft();
+
+
+    while (roomba.frontIsClear()) {
+		height++; 
+		roomba.move();
+	}
+    roomba.turnLeft();
+ 
+    int x = width;
+    int y= height;
+     
+	while (x>0 && y>0) {
+     //Moving East/West
+ 	for (int i=0; i<x; i++) {
+	 roomba.move(); 
+	 pickBeepers();
+ }
+   x--;
+   roomba.turnLeft();
+
+	for (int i=0; i<y; i++) {
+	 roomba.move(); 
+	 pickBeepers();
+   }
+	y--;
+	roomba.turnLeft();
+ }  // end of while 
+} // end of method 
+
+private void pickBeepers() {
+	area++;
+	int pileCount = 0;
+
+	while (roomba.nextToABeeper()) {
+		roomba.pickBeeper();
+		pileCount++;
+		totalBeepers++;
 	}
 
-	// declared here so it is visible in all the methods!
-	private Robot roomba;
-
-	// You will need to add many variables!!
-
-
-	public int cleanRoom(String worldName, int startX, int startY) {
-
-		// A new Robot should be constructed and assigned to the global (instance) variable named roomba that is declared above.
-        // Make sure it starts at startX and startY location.
-
-		World.readWorld(worldName);
-		World.setVisible(true);
-
-
-		/** This section will have all the logic that takes the Robot to every location
-		 * and cleans up all piles of beepers. Think about ways you can break this
-		 * large, complex task into smaller, easier to solve problems.
-		 */
-
-		// the line below causes a null pointer exception
-		// what is that and why are we getting it?
-		roomba.move();
-
-
-		int totalBeepers = 0; // Need to move this somewhere else.
-        // This method should return the total number of beepers cleaned up.
-		return totalBeepers;
+	if (pileCount > 0) {
+		piles++;
+		if (pileCount > largestPile) {
+			largestPile = pileCount;
+			largestX = roomba.avenue();
+			largestY = roomba.street();
+		}
 	}
 }
+
+public void report() {
+	double averagePile = 0;
+	double percentageDirty = 0;
+
+	if (piles > 0) {
+		averagePile = (double) totalBeepers / piles;		
+	}
+
+	if (area > 0) {
+		percentageDirty = (double) piles / area;
+	}
+
+	// print all the statistics we have collected
+	System.out.println("Printing report");
+	System.out.println("Area of room: " + area);
+	System.out.println("Number of piles: " + piles);
+	System.out.println("Total number of beepers: " + totalBeepers);
+    System.out.println("Largest Pile: " + largestPile);
+
+	System.out.println("Largest Pile: Street " + largestY);
+	System.out.println("Largest Pile: Avenue " + largestX);
+
+	System.out.println("Average pile size: " + averagePile);
+	System.out.println("Percentage Dirty: " + percentageDirty);
+}
+
+}
+
+
+
+
